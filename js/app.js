33a0843bfc126224831bd9e370b4920063a75b4f
nimah69 <nimakh29300@gmail.com>
2026-08-29 20:44:07 +0330

    Connect UI module

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 // SupermarketPOS
 // Application Entry Point
+// Stage 4
 
 'use strict';
+
+import { showWelcomeMessage } from './ui.js';
 
 console.log('SupermarketPOS application started successfully.');
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM loaded successfully.');
+
+    showWelcomeMessage();
 });
